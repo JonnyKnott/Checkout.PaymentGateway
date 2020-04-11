22bb489633f3ade2fb32bf7a6d70162ebe1bee73
jonjonjabon <jonny.knott.jk@gmail.com>
2020-04-11 17:18:33 +0100

    *Missed from previous checkin*
    - Dockerfile update and csproj changes ommited accidentally

--- test/Checkout.PaymentGateway.WebApi.Client.Integration.Test/PaymentClientTests.cs
+++ test/Checkout.PaymentGateway.WebApi.Client.Integration.Test/PaymentClientTests.cs
@@ -1,13 +1,22 @@
 using System;
+using Checkout.PaymentGateway.WebApi.Client.Payment;
 using Xunit;
 
 namespace Checkout.PaymentGateway.WebApi.Client.Integration.Test
 {
-    public class UnitTest1
+    public class PaymentClientTests
     {
-        [Fact]
-        public void Test1()
+        private readonly IPaymentClient _client;
+
+        public PaymentClientTests(IPaymentClient client)
         {
+            _client = client;
+        }
+
+        [Fact]
+        public void PaymentClient_DI_Should_Provide_Injectable_Client()
+        {
+            Assert.True(true);
         }
     }
 }
--- test/Checkout.PaymentGateway.WebApi.Client.Integration.Test/TestStartup.cs
+++ test/Checkout.PaymentGateway.WebApi.Client.Integration.Test/TestStartup.cs
@@ -1,25 +1,43 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
+using Checkout.PaymentGateway.WebApi.Client.Payment;
+using Checkout.PaymentGateway.WebApi.Client.Payment.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Xunit;
 using Xunit.Abstractions;
 using Xunit.DependencyInjection;
 
 [assembly: TestFramework(
-    "Checkout.PaymentGateway.WebApi.Client.Test.TestStartup",
-    "Checkout.PaymentGateway.WebApi.Client.Test")]
-namespace Checkout.PaymentGateway.WebApi.Client.Test
+    "Checkout.PaymentGateway.WebApi.Client.Integration.Test.TestStartup",
+    "Checkout.PaymentGateway.WebApi.Client.Integration.Test")]
+namespace Checkout.PaymentGateway.WebApi.Client.Integration.Test
 {
     public class TestStartup : DependencyInjectionTestFramework
     {
         public TestStartup(IMessageSink messageSink) : base(messageSink)
         {
         }
+        
+        protected override IHostBuilder CreateHostBuilder(AssemblyName assemblyName) =>
+            base.CreateHostBuilder(assemblyName)
+                .ConfigureHostConfiguration(builder =>
+                {
+                    builder.AddInMemoryCollection(new List<KeyValuePair<string, string>>
+                    {
+                        new KeyValuePair<string, string>("PaymentClient:Endpoint", "http://localhost:9002/")
+                    });
+                })
+                .ConfigureServices((context, services) => ConfigureServices(context, services));
 
-        protected void ConfigureServices(IServiceCollection services)
+        protected void ConfigureServices(HostBuilderContext context, IServiceCollection services)
         {
-            var path = Directory.GetCurrentDirectory();
-            
+            var clientConfiguration =
+                context.Configuration.GetSection("PaymentClient").Get<PaymentClientConfiguration>();
+
+            services.AddPaymentServiceClient(clientConfiguration);
         }
     }
 }
